6865f6731de3802b79a07870ce18a75f6cba67a1
ronitafter <54575125+ronitafter@users.noreply.github.com>
2022-03-11 16:12:24 +0200

    CrossOrigin

--- spring-couponSystem3/src/main/java/com/ronit/controllers/CompanyController.java
+++ spring-couponSystem3/src/main/java/com/ronit/controllers/CompanyController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,6 +36,7 @@ import com.ronit.exceptions.LoginException;
 
 @RestController
 @RequestMapping("/company")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CompanyController extends ClientController {
 
 	@Autowired
