ae21baec62c12a69f730ad4089c09dadf656df2e
ronitafter <54575125+ronitafter@users.noreply.github.com>
2022-03-18 22:57:33 +0200

    jwt and security 2

--- spring-couponSystem3/src/main/java/com/ronit/utils/JwtUtils.java
+++ spring-couponSystem3/src/main/java/com/ronit/utils/JwtUtils.java
@@ -1,34 +1,49 @@
 package com.ronit.utils;
 
+import java.util.Arrays;
+
 import java.util.Collection;
+
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
+
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
-
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwsHeader;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
 
-@Service
-public class JwtUtils {
+
+
 /*
  * 
- * this class has utility methods corresponding to the
-  JWT: create JWT, check if JWT is valid. Currently we will be defining
- * only the methods to create the JWT. Other methods we will be creating later
- * as needed
+ * this class has utility methods corresponding to the JWT: create JWT, check if
+ * JWT is valid. Currently we will be defining only the methods to create the
+ * JWT. Other methods we will be creating later as needed
  * 
  * 
  */
+@Service
+public class JwtUtils {
+	
+
 
 	private String secret;
 	private int jwtExpirationInMs;
-	
+
 	@Value("${jwt.secret}")
 	public void setSecret(String secret) {
 		this.secret = secret;
@@ -38,8 +53,9 @@ public class JwtUtils {
 	public void setJwtExpirationInMs(int jwtExpirationInMs) {
 		this.jwtExpirationInMs = jwtExpirationInMs;
 	}
-	
-	private String generateTokens(UserDetails userDetails) {
+
+	// generate token for user
+	public String generateToken(UserDetails userDetails) {
 		Map<String, Object> claims = new HashMap<>();
 		Collection<? extends GrantedAuthority> roles = userDetails.getAuthorities();
 		if (roles.contains(new SimpleGrantedAuthority("ROLE_ADMIN"))) {
@@ -57,8 +73,40 @@ public class JwtUtils {
 				.setExpiration(new Date(System.currentTimeMillis() + jwtExpirationInMs)).signWith(SignatureAlgorithm.HS512, secret).compact();
 	}
 
-}
+	public boolean validateToken(String authToken) {
+		try {
+			// Jwt token has not been tampered with
+			Jws<Claims> claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(authToken);
+			return true;
+		} catch (SignatureException | MalformedJwtException | UnsupportedJwtException | IllegalArgumentException ex) {
+			throw new BadCredentialsException("INVALID_CREDENTIALS", ex);
+		} catch (ExpiredJwtException ex) {
+			throw new Exception(header, claims, "Token has Expired", ex);
+
+
+		}
+	}
+	
+	public String getUsernameFromToken(String token) {
+		Claims claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+
+		return claims.getSubject();
+	}
+
+	public List<SimpleGrantedAuthority> getRolesFromToken(String authToken) {
+		List<SimpleGrantedAuthority> roles = null;
+		Claims claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(authToken).getBody();
+		Boolean isAdmin = claims.get("isAdmin", Boolean.class);
+		Boolean isUser = claims.get("isUser", Boolean.class);
+		if (isAdmin != null && isAdmin == true) {
+			roles = Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"));
+		}
+		if (isUser != null && isUser == true) {
+			roles = Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+		}
+		return roles;
+	}
 
 	
 
-
+}
